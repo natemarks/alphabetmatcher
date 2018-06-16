7e47deed54b1745ef5f82e8a827f36a2e9b65667
Nate Marks <npmarks@gmail.com>
2018-06-16 04:54:37 -0400

    UPdated app.py to improve the docs and remove some unneeded imports

--- alphabetmatcher/alphabetmatcher/app.py
+++ alphabetmatcher/alphabetmatcher/app.py
@@ -1,10 +1,26 @@
 """
-rest API interface for alphabetmatcher
+This is the entry point for the alphabetmatcher application.
+
+Main uses Flask to create a REST listener interface.  Data provided via POST is passed to alphabetmatcher.Matcher at
+init. The output of Matcher.success() is returned via the POST response.
+
+The application can be manually tested by running app.py then running the command below on the same host.
+
+curl -i -X POST -H "Content-Type:application/json"  \
+http://localhost:5000/matcher -d '{"matcher_input":"abcdefghijklmnopqrstuvwxyz"}'
+
+This should produce a successsful test wiht the string "True"  like this:
+
+HTTP/1.0 200 OK
+Content-Type: text/html; charset=utf-8
+Content-Length: 4
+Server: Werkzeug/0.14.1 Python/2.7.15
+Date: Sat, 16 Jun 2018 00:20:03 GMT
+
+True
 """
 
-import datetime
 import json
-import time
 
 from flask import Flask, request
 from alphabetmatcher import Matcher
--- tests/test_alphabetmatcher.py
+++ tests/test_alphabetmatcher.py
@@ -20,17 +20,20 @@ def test_exact_match():
     dd = Matcher('abcdefghijklmnopqrstuvwxyz')
     assert dd.success()
 
+
 def test_case_mixing():
     from alphabetmatcher.alphabetmatcher import Matcher
 
     dd = Matcher('abcdefGhijklmnopqrStuvwxyz')
     assert dd.success()
 
+
 def test_reorder_and_junk():
     from alphabetmatcher.alphabetmatcher import Matcher
 
     dd = Matcher('CBAhjsvdf734y4tu9820h%$%$&%defGhijklmnopqrStuvwxyz')
     assert dd.success()
+
 
 def test_repeats():
     from alphabetmatcher.alphabetmatcher import Matcher
